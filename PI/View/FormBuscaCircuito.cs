56052cc133787222625572d761b6ec760d1d29fa
kaio_ <kaio_rossi@hotmail.com>
2020-06-06 12:27:01 -0300

    Teste

---
+++ PI/View/FormBuscaCircuito.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PI.View
+{
+    public partial class FormBuscaCircuito : Form
+    {
+        public FormBuscaCircuito()
+        {
+            InitializeComponent();
+        }
+    }
+}
